8e7e8ec442c7ea722f4ae5402777577525c04b96
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-02-07 14:00:46 +0200

    mvoed build folder

--- mainapp.py
+++ mainapp.py
@@ -1,6 +1,11 @@
 from website import create_app
+from flask_frozen import Freezer
+
 
 app = create_app()
+freezer = Freezer(app)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+    freezer.freeze()
