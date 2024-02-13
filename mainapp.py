1e6f48f1fe46852c75bae4e543c1083eb5842eb3
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-02-13 10:04:11 +0200

    Deploying website to render

--- mainapp.py
+++ mainapp.py
@@ -3,9 +3,9 @@ from flask_frozen import Freezer
 
 
 app = create_app()
-freezer = Freezer(app)
+# freezer = Freezer(app)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    freezer.freeze()
+    # freezer.freeze()
